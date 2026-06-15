2801611c68359bc60ec1aee93d6129757f496b0e
Min12834 <127608855+Min12834@users.noreply.github.com>
2026-06-16 00:03:22 +0200

    Update TOKEN variable to use a placeholder
    Replaced sensitive token with a placeholder.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 import requests
 import websockets
 
-TOKEN = "MTI3NTE1MjM2Mjc4ODQ4NzI5NQ.GsJ2_-.oR03x7pqp3_SXI4YAAYgv3-7xeAFdnVupsl8Lg"
+TOKEN = "token"
 STATUS = "dnd"  # online / dnd / idle
 CUSTOM_STATUS = "✅ 1.5$ Rust, cheatvault.net"
 USE_EMOJI = False
